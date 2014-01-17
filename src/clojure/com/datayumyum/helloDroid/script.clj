b9069d05483f66dee7afaf7627a0186aa4c2fd7a
SonnyTo <sto@csicorporatecard.com>
2014-01-16 20:54:55 -0500

    aa

--- src/clojure/com/datayumyum/helloDroid/script.clj
+++ src/clojure/com/datayumyum/helloDroid/script.clj
@@ -1,11 +1,16 @@
 (ns com.datayumyum.helloDroid.main)
 
-(def main-layout [:linear-layout {:orientation :vertical}
-                  [:edit-text {:hint "Event name3"}]])
+(require 'neko.notify)
+
+;; (def main-layout [:linear-layout {:orientation :vertical}
+;;                   [:edit-text {:hint "Event name3"}]
+;;                   [:button {:text "i'm a button"
+;;                             :on-click (fn [_] (neko.notify/toast "toast to you"))}]])
 
 
-    (on-ui
-     (set-content-view! a
-      (make-ui main-layout) ))
+
+;;     (on-ui
+;;      (set-content-view! a
+;;       (make-ui main-layout) ))
 
 
