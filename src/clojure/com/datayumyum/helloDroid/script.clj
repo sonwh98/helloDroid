edbd8972c04fac7aa91fb4e7c3f2e59481c46fc7
SonnyTo <sto@csicorporatecard.com>
2014-01-27 23:45:57 -0500

    commit

--- src/clojure/com/datayumyum/helloDroid/script.clj
+++ src/clojure/com/datayumyum/helloDroid/script.clj
@@ -1,16 +1,41 @@
 (ns com.datayumyum.helloDroid.main)
 
 (require 'neko.notify)
-
-;; (def main-layout [:linear-layout {:orientation :vertical}
-;;                   [:edit-text {:hint "Event name3"}]
-;;                   [:button {:text "i'm a button"
-;;                             :on-click (fn [_] (neko.notify/toast "toast to you"))}]])
-
-
-
-;;     (on-ui
-;;      (set-content-view! a
-;;       (make-ui main-layout) ))
+(require 'com.rabbitmq.client.ConnectionFactory)
+ (def main-layout [:linear-layout {:orientation :vertical}
+                   [:edit-text {:hint "Event name3"}]
+                   [:button {:text "i'm a button"
+                             :on-click (fn [_] (neko.notify/toast "toast to me"))}]])
 
 
+
+     (on-ui
+     (set-content-view! a
+      (make-ui main-layout) ))
+
+(import '(com.rabbitmq.client ConnectionFactory Connection Channel MessageProperties QueueingConsumer))
+(defn sendQueue [msg]
+  (do
+    (def factory (ConnectionFactory.))
+    (.setHost factory "obi.kaicode.com")
+    (def connection (.newConnection factory))
+    (def channel (.createChannel connection))
+    (def queue "han.printer")
+    (.queueDeclare channel queue true false false nil)
+    (.basicPublish channel "" queue MessageProperties/PERSISTENT_TEXT_PLAIN (.getBytes msg))    
+    )
+)
+
+(defn readQeue []
+    (def factory (ConnectionFactory.))
+    (.setHost factory "obi.kaicode.com")
+    (def connection (.newConnection factory))
+    (def channel (.createChannel connection))
+    (def queue "han.printer")
+    (.queueDeclare channel queue true false false nil)
+    (def consumer (QueueingConsumer. channel))
+    (.basicConsume channel queue true consumer)
+    (def delivery (.nextDelivery consumer))
+    (def message (String. (.getBody delivery)))
+    (println message)
+)
